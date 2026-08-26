9cd6644afa96a16efb3fca01f6593098bd7bb3dc
Shaheer <frostbyte.a.b@gmail.com>
2026-08-26 18:34:09 +0500

    Fixed CORS

--- server/main.py
+++ server/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://shaheer-frostbyte.github.io/Live-Menu/"
+    "https://shaheer-frostbyte.github.io"
     # Another origin: hosted frontend url
 ]
 app.add_middleware(
